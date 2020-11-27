60d62ea87142bc12bbdb59af0d78a8f12b302432
Florian Wartner <florian@wartner.io>
2020-11-27 10:35:41 +0100

    WIP

---
+++ database/migrations/create_subscriptions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+
+            $table->string('paddle_user_id');
+            $table->string('paddle_subscription_id');
+            $table->string('paddle_plan_id');
+            $table->string('paddle_checkout_id')->nullable();
+
+            $table->enum('status', ['active', 'trialing', 'past_due', 'paused', 'deleted']);
+            $table->timestamp('cancelled_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
--- src/KanuuServiceProvider.php
+++ src/KanuuServiceProvider.php
@@ -17,6 +17,7 @@ class KanuuServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/kanuu.php', 'kanuu');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->app->singleton(Kanuu::class, function () {
             return $this->resolveKanuuManager();
@@ -27,6 +28,10 @@ class KanuuServiceProvider extends ServiceProvider
 
     protected function publishConfig()
     {
+        $this->publishes([
+            __DIR__.'/../stubs/KanuuServiceProvider.stub' => app_path('Providers/KanuuServiceProvider.php'),
+        ], 'kanuu-provider');
+
         $this->publishes([
             __DIR__ . '/../config/kanuu.php' => config_path('kanuu.php'),
         ], ['config', 'kanuu-config']);
