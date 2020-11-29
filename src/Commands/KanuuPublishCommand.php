66a5cb02de0d3a3c1ca833c47073d06729f468ea
Loris Leiva <loris.leiva@gmail.com>
2020-11-29 15:03:25 +0000

    wip

--- src/Commands/KanuuPublishCommand.php
+++ src/Commands/KanuuPublishCommand.php
@@ -11,11 +11,52 @@ class KanuuPublishCommand extends GeneratorCommand
 
     public function handle()
     {
-        $this->line('');
+        $this->call('make:model', [
+            'name' => 'Subscription',
+            '--migration' => true,
+            '--factory' => true,
+        ]);
+
+        // $this->line(
+        //     $this->qualifyClass('Providers/KanuuServiceProvider')
+        // );
     }
 
     protected function getStub()
     {
         return '';
+    }
+
+    protected function createStub($rawName, $stubFile): bool
+    {
+        $name = $this->qualifyClass($rawName);
+
+        if (! $this->isForced() && $this->alreadyExists($rawName)) {
+            $this->error($name . ' already exists!');
+
+            return false;
+        }
+
+        $path = $this->getPath($name);
+        $this->makeDirectory($path);
+        $this->files->put($path, $this->buildStub($name, $stubFile));
+
+        $this->info($this->type.' created successfully.');
+
+        return true;
+    }
+
+    protected function buildStub($name, $stubFile)
+    {
+        $stub = $this->files->get($stubFile);
+        $stub = $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+        $stub = $this->sortImports($stub);
+
+        return $stub;
+    }
+
+    protected function isForced()
+    {
+        return $this->hasOption('force') && $this->option('force');
     }
 }
--- tests/KanuuPublishCommandTest.php
+++ tests/KanuuPublishCommandTest.php
@@ -2,7 +2,11 @@
 
 namespace Kanuu\Laravel\Tests;
 
+use Carbon\Carbon;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 use Kanuu\Laravel\Commands\KanuuPublishCommand;
 
 /**
@@ -10,13 +14,51 @@ use Kanuu\Laravel\Commands\KanuuPublishCommand;
  */
 class KanuuPublishCommandTest extends TestCase
 {
+    /** @var string */
+    protected static $tmpDir = __DIR__ . '/tmp';
+
+    /** @var Filesystem  */
+    protected $files;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->files = new Filesystem;
+        $this->files->copyDirectory($this->app->basePath(), static::$tmpDir);
+        $this->app->setBasePath(static::$tmpDir);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->files->deleteDirectory(static::$tmpDir);
+    }
+
     /** @test */
     public function dummy()
     {
-        Artisan::call('kanuu:publish');
-        dd(Artisan::output());
-        $this->artisan('kanuu:publish')
-            ->expectsOutput('')
-            ->run();
+        $this->artisan('kanuu:publish');
+
+        $this->assertBasePathFileExists('app/models/Subscription.php');
+        $this->assertBasePathFileExists('database/factories/SubscriptionFactory.php');
+        $this->assertContainsEquals('create_subscriptions_table', $this->getMigrations());
+    }
+
+    protected function assertBasePathFileExists($filename)
+    {
+        $this->assertFileExists($this->app->basePath($filename));
+    }
+
+    protected function getMigrations(): Collection
+    {
+        $filenames = $this->files->glob(
+            $this->app->basePath('database/migrations/*.php')
+        );
+
+        return collect($filenames)->map(function ($filename) {
+            return (string) Str::of($filename)->afterLast('/')->substr(18, -4);
+        });
     }
 }
